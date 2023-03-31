cca3feaa550a9ea1f75ea70cf60f7c2e080bd461
Akshat2106 <akshatpatidar2106@gmail.com>
2023-03-31 17:01:56 +0530

    task 2

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -1,48 +1,91 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import DummyScreen from '../Layout/DummyScreen';
 import './Login.Module.css'
 
 const Login = () => {
     let enteredpass = useRef();
     let enteredEmail = useRef();
     let enteredConfirmPass = useRef();
+    const [login, setLogin] = useState(false);
+    const [token, setToken] = useState(null);
 
-    const submitHandler = async(e) => {
+    const submitHandler = async (e) => {
         e.preventDefault();
-        if (enteredpass.current.value===enteredConfirmPass.current.value) {
-           let responce=await  fetch(
-                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD6kzxPr6_3Yh9cSy0_nbCijFvmr538c3w',
-                {
-                  method: 'POST',
-                  body: JSON.stringify({
-                    email: enteredEmail.current.value,
-                    password: enteredpass.current.value,
-                    returnSecureToken: true,
-                  }),
-                  headers: {
-                    'Content-Type': 'application/json',
-                  },
+        if (!login) {
+            if (enteredEmail.current.value.length > 0 && enteredpass.current.value.length > 0 && enteredConfirmPass.current.value.length > 0) {
+                if (enteredpass.current.value === enteredConfirmPass.current.value) {
+                    let responce = await fetch(
+                        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD6kzxPr6_3Yh9cSy0_nbCijFvmr538c3w',
+                        {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                email: enteredEmail.current.value,
+                                password: enteredpass.current.value,
+                                returnSecureToken: true,
+                            }),
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                        }
+                    )
+
+                    if (responce.ok) {
+                        let data = await responce.json();
+                        console.log("Authantication Token:", data.idToken);
+                        alert("User has signed Up")
+                        console.log("User has signed Up");
+                        setToken(data.idToken);
+                    } else {
+                        let errorMessage = 'Authentication failed!';
+                        alert(errorMessage);
+                    }
                 }
-              )
-              
-              if (responce.ok) {
-                 let data=await responce.json();
-                 console.log("Authantication Token:",data.idToken);
-                 alert("User has signed Up")
-                 console.log("User has signed Up");
+                else {
+                    alert("Password and confirm passwords are not matching")
+                }
+            } else {
+                alert("please fill all the data")
+            }
+        } else {
+            if (enteredEmail.current.value.length > 0 && enteredpass.current.value.length > 0) {
+                let responce = await fetch(
+                    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD6kzxPr6_3Yh9cSy0_nbCijFvmr538c3w',
+                    {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            email: enteredEmail.current.value,
+                            password: enteredpass.current.value,
+                            returnSecureToken: true,
+                        }),
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    }
+                )
+
+                if (responce.ok) {
+                    let data = await responce.json();
+                    setToken(data.idToken);
+                    console.log("Authantication Token:", token);
+                    alert("Logged In Successfully")
+                    console.log("Logged In Successfully");
                 } else {
                     let errorMessage = 'Authentication failed!';
                     alert(errorMessage);
                 }
             }
-            else{
-                alert("Password and confirm passwords are not matching")
+            else {
+                alert("please fill all the data")
             }
         }
-            
+
+
+    }
+
     return (
         <div>
-            <section className="auth">
-                <h2 className='my-3'>Sign Up</h2>
+            {!token && <section className="auth">
+                <h2 className='my-3'>{!login ? "Sign Up" : "Log In"}</h2>
                 <form >
                     <div className="control">
                         <input type='email' id='email' placeholder='Email' ref={enteredEmail} />
@@ -56,7 +99,7 @@ const Login = () => {
                             ref={enteredpass}
                         />
                     </div>
-                    <div className="control">
+                    {!login && <div className="control">
                         <input
                             type='password'
                             id='password'
@@ -64,18 +107,23 @@ const Login = () => {
                             placeholder='Confirm Password'
                             ref={enteredConfirmPass}
                         />
-                    </div>
+                    </div>}
                     <div >
-                        <button className="btn btn-primary border w-100" onClick={submitHandler}>Sign Up</button>
+                        <button className="btn btn-primary border w-100" onClick={submitHandler}>{!login ? "Sign Up" : "Login"}</button>
                     </div>
                 </form>
-            <div>
-                <button type="button" className="btn btn-outline-success my-3 w-100">Have an account?Login</button>
-            </div>
-            </section>
+                <div>
+                    <button
+                        onClick={() => { setLogin(!login) }}
+                        type="button"
+                        className="btn btn-outline-success my-3 w-100">
+                        {!login ? "Have an account?Login" : "Don't have an account?Sign up"}
+                    </button>
+                </div>
+            </section> }
+            {token && <DummyScreen/>}
         </div>
     )
 }
 
 export default Login
-
